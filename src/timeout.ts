c011455c8bb2e00e34e3f94091a525a9bb58f6ec
Pawel Badenski <pawel.badenski@gmail.com>
2021-06-28 13:56:44 +0100

    feat: disable timeout if time is undefined
    useful for context#ask method

--- src/timeout.ts
+++ src/timeout.ts
@@ -2,8 +2,12 @@ const wait = (timeInMillis: number): Promise<void> => {
   return new Promise<void>(resolve => setTimeout(() => resolve(), timeInMillis));
 };
 
-export const timeout = async <T>(promise: Promise<T>, timeoutInMs: number): Promise<T> =>
-  Promise.race([
+export const timeout = async <T>(promise: Promise<T>, timeoutInMs: number): Promise<T> => {
+  if (timeoutInMs === undefined) {
+    return promise;
+  }
+  return Promise.race([
     promise,
     wait(timeoutInMs).then(() => Promise.reject(new Error(`Timed out after ${timeoutInMs} ms`)))
   ]);
+};
